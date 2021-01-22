3088de9a34afaba30ae355771110dd811bda2011
Shane ONeill <oneill.shanepatrick@gmail.com>
2021-01-22 00:12:30 -0500

    styling, title, font, img

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,68 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-row id="title-row">
+      <b-col class="text-right">
+        <img alt="Riley" id="riley" src="./assets/riley.png" />
+      </b-col>
+      <b-col class="text-left">
+        <h1>Riley's</h1>
+        <h1>Prize</h1>
+        <h1>Shop</h1>
+      </b-col>
+      <!-- TODO
+      <b-col>
+        How many tickets do you have?
+      </b-col> -->
+    </b-row>
+    <b-row>
+      <b-col>
+        <Prize
+          title="First Prize"
+          imgSrc="https://via.placeholder.com/250"
+          :numberOfTickets="5"
+          url=""
+        />
+      </b-col>
+      <b-col>
+        <Prize
+          title="Second Prize"
+          imgSrc="https://via.placeholder.com/250"
+          :numberOfTickets="10"
+          url=""
+        />
+      </b-col>
+      <b-col>
+        <Prize
+          title="Third Prize"
+          imgSrc="https://via.placeholder.com/250"
+          :numberOfTickets="15"
+          url=""
+        />
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import Vue from "vue";
+import Prize from "./components/Prize.vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
-// Import Bootstrap an BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+// Import Bootstrap an BootstrapVue CSS files
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
+// Make BootstrapVue available project
+Vue.use(BootstrapVue);
+// Install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Prize,
+  },
+};
 </script>
 
 <style>
@@ -35,5 +73,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#title-row{
+  margin-bottom: 50px;
+}
+
+h1 {
+  margin-bottom: 25px;
+  color: white;
+  font-family: "Pacifico", cursive;
+  font-size: 5rem;
+}
+
+#riley {
+  max-height: 350px;
 }
 </style>
---
+++ src/components/Prize.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <b-card :title= title class="prize-card">
+      <b-row>
+        <b-col>
+          <img :src="imgSrc" /><br />
+        </b-col>
+      </b-row>
+      <b-row class="ticket-row">
+        <b-col>
+          {{ numberOfTickets }} Tickets!
+        </b-col>
+      </b-row>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Prize",
+  props: {
+    title: String,
+    imgSrc: String,
+    numberOfTickets: Number,
+    url: String,
+  },
+};
+</script>
+
+<style scoped>
+
+.prize-card {
+  max-width: 20rem;
+  border-radius: 15px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.ticket-row {
+  margin-top: 15px;
+  font-weight: bold;
+}
+
+</style>
