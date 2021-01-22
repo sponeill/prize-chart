30581ff8bf104d7449b411e1faf52dc5d48760ce
Shane ONeill <oneill.shanepatrick@gmail.com>
2021-01-21 23:18:45 -0500

    add bootstrap vue, page title

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,18 @@
 </template>
 
 <script>
+import Vue from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 export default {
   name: 'App',
